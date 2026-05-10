20659fe9d7dc7a8c1b0f8e76e4808d49a0470e70
comicmuse <14880722+comicmuse@users.noreply.github.com>
2026-05-10 17:56:29 +0100

    fix: dart format plan_provider.dart

--- mobile/lib/providers/plan_provider.dart
+++ mobile/lib/providers/plan_provider.dart
@@ -2,7 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/planned_workout.dart';
 import 'auth_provider.dart';
 
-final planProvider = FutureProvider.autoDispose<List<PlannedWorkout>>((ref) async {
+final planProvider = FutureProvider.autoDispose<List<PlannedWorkout>>((
+  ref,
+) async {
   ref.watch(authProvider);
   final api = ref.read(apiServiceProvider);
   return api.getPlannedWorkouts();
